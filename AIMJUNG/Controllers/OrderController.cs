e634eee3b8f9c72581c36343e81b1b2092fa39bd
koochat <koochat2330@gmail.com>
2020-10-13 13:15:12 +0700

    first commit

---
+++ AIMJUNG/Controllers/OrderController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AIMJUNG.Controllers
+{
+    public class OrderController : Controller
+    {
+        // GET: Order
+        public ActionResult Pork()
+        {
+            return View();
+        }
+
+        public ActionResult Chicken()
+        {
+            return View();
+        }
+
+        public ActionResult Fish()
+        {
+            return View();
+        }
+
+        public ActionResult Beef()
+        {
+            return View();
+        }
+
+        public ActionResult Drink()
+        {
+            return View();
+        }
+    }
+}
